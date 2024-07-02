8bc13d0462f3001b2673b152afa3620e96f2bbdd
djdjz7 <djdjz7@icloud.com>
2024-07-02 08:20:52 +0800

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn handle_input(command: &str, args: Vec<&str>) {
         "echo" => println!("{}", args.join(" ")),
         "type" => {
             let builtin_commands = ["exit","echo","type"];
-            if builtin_commands.contains(&args[1]) {
+            if builtin_commands.contains(&args[0]) {
                 println!("{} is a shell builtin", command);
             }
             else {
